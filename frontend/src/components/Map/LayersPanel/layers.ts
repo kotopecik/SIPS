69dbf3b450d74c32f367758d91e66a81cdc0152f
DeadEndjke <perceptionarmy@icloud.com>
2024-02-06 05:09:54 +0700

    add layer switch

---
+++ frontend/src/components/Map/LayersPanel/layers.ts
@@ -0,0 +1,20 @@
+import {ILayer} from "@/components/Map/LayersPanel/ILayer";
+
+export const layers:ILayer[] = [
+    {
+        name: 'Standart map',
+        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    },
+    {
+        name: 'Transport map',
+        url: 'https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png',
+    },
+    {
+        name: 'ESRI map',
+        url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    },
+    {
+        name: 'Monochrome map',
+        url: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
+    },
+]
