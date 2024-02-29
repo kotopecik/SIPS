54d7b44a4af3058f3cb1f0fdef0401c6433c507c
DeadEndjke <perceptionarmy@icloud.com>
2024-02-29 10:41:34 +0700

    simplified map.tsx

--- frontend/src/components/Map/Map.tsx
+++ frontend/src/components/Map/Map.tsx
@@ -1,19 +1,16 @@
 import './Map.scss'
-import {MapContainer, TileLayer, Marker, Popup, Polygon, Polyline, Tooltip} from "react-leaflet";
+import {MapContainer, TileLayer} from "react-leaflet";
 import {LatLngExpression, LatLngLiteral} from "leaflet";
 import {useAppDispatch, useAppSelector} from "@/hooks/hook";
-import {IRegion} from "@/interfaces/IRegion";
 import Coords from "@/components/Map/Coords/Coords";
 import Settings from "@/components/Settings";
 import Calendar from "@/components/Calendar";
 import Ruler from "@/components/Map/Ruler/Ruler";
-import {RulerCalculations} from "@/utils/RulerCalculations";
-import {icons} from "@/data/Icons";
 import {addRulerMarker, addRulerMarkerPos} from "@/store/ruler/ruler-slice";
-import {IMarker} from "@/interfaces/IMarker";
 import Cursor from "@/components/Cursor/Cursor";
 import Regions from "@/components/Map/Regions/Regions";
 import NatureReserves from "@/components/Map/NatureReserves/NatureReserves";
+import RulerMarkers from "@/components/Map/RulerMarkers/RulerMarkers";
 
 //спасибо центру за это
 const CENTR:LatLngExpression = [54.84643545576913, 83.05183410644533];
@@ -21,11 +18,6 @@ const CENTR:LatLngExpression = [54.84643545576913, 83.05183410644533];
 
 const Map = () => {
 
-  const purpleOptions = { color: 'red' }
-
-
-  const rulerMarkers:IMarker[] = useAppSelector(state => state.ruler).rulerMarkers
-  const rulerMarkersPos:LatLngExpression[] = useAppSelector(state => state.ruler).rulerMarkersPos
   const layer:string = useAppSelector(state => state.map).layer
   const isRulerActive:boolean = useAppSelector(state => state.ruler).isRulerActive
   const mousePos:LatLngLiteral = useAppSelector(state => state.cursor).mousePos
@@ -55,25 +47,11 @@ const Map = () => {
               maxBounds={[[-110, -170], [100, 200]]}
               dragging = {dragging}
           >
-              <TileLayer
-                  url={layer}
-              />
+              <TileLayer url={layer} />
 
-              {rulerMarkers.map((marker, index) => (
-                  <Marker key = {index} position = {marker.position} icon = {icons().rulerIcon} />
-              ))}
-
-              {RulerCalculations.iterateLatLng(rulerMarkersPos).map((marker) => (
-                  <Marker position = {marker.pos} icon = {icons().emptyIcon}>
-                      <Tooltip permanent>{marker.title}</Tooltip>
-                  </Marker>
-              ))}
-
-              <Polyline pathOptions={purpleOptions} positions={rulerMarkersPos} />
-
+              <RulerMarkers />
               <Regions />
               <NatureReserves />
-
               <Coords />
               <Settings />
               <Calendar />
---
+++ frontend/src/components/Map/RulerMarkers/RulerMarkers.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Marker, Polyline, Tooltip} from "react-leaflet";
+import {icons} from "@/data/Icons";
+import {RulerCalculations} from "@/utils/RulerCalculations";
+import {IMarker} from "@/interfaces/IMarker";
+import {useAppSelector} from "@/hooks/hook";
+import {LatLngExpression} from "leaflet";
+
+const RulerMarkers = () => {
+    const purpleOptions = { color: 'red' }
+    const rulerMarkers:IMarker[] = useAppSelector(state => state.ruler).rulerMarkers
+    const rulerMarkersPos:LatLngExpression[] = useAppSelector(state => state.ruler).rulerMarkersPos
+    return (
+        <>
+            {rulerMarkers.map((marker, index) => (
+                <Marker key = {index} position = {marker.position} icon = {icons().rulerIcon} />
+            ))}
+
+            {RulerCalculations.iterateLatLng(rulerMarkersPos).map((marker) => (
+                <Marker position = {marker.pos} icon = {icons().emptyIcon}>
+                    <Tooltip permanent>{marker.title}</Tooltip>
+                </Marker>
+            ))}
+
+            <Polyline pathOptions={purpleOptions} positions={rulerMarkersPos} />
+        </>
+    );
+};
+
+export default RulerMarkers;
