33685326ba33b70a72d5d24857e77ff9491c429f
Eve <evaslobodyan8@gmail.com>
2024-03-23 14:39:56 +0700

    обновлены Dockerfile и docker-compose файлы

---
+++ backend/CRUDAPI/regauth/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from datetime import datetime
+
+
+# 3
+class Todo(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    description = models.TextField(max_length=400, null=False)
+    completed = models.BooleanField(default=False)
+    date = models.DateTimeField(default=datetime.utcnow)
+
+    def __str__(self) -> str:
+        return self.name
