750b3af82971343b4aeb1372b4e3594f2191405f
SimpleIN1 <zimforshint_akross@mail.ru>
2024-04-15 18:40:32 +0700

    added del tables and potgresclass

--- backend/Utils/database_setting.py
+++ backend/Utils/database_setting.py
@@ -8,3 +8,11 @@ class DataBaseSetting:
     dbname: str
     user: str
     password: str
+
+    def get_raw_connection(self):
+        pass
+
+
+class PostgresDataBaseSetting(DataBaseSetting):
+    def get_raw_connection(self) -> str:
+        return f'postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.dbname}'
