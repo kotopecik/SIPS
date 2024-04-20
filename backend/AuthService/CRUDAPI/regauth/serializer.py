4cf0dccc2b0821b73ec82385a21f36dd1581a408
Eve <evaslobodyan8@gmail.com>
2024-04-20 13:07:08 +0700

    The validation field is placed separately

--- backend/AuthService/CRUDAPI/regauth/serializer.py
+++ backend/AuthService/CRUDAPI/regauth/serializer.py
@@ -1,7 +1,19 @@
-import pprint
-
 from rest_framework import serializers
 from .models import UserModel
+
+class PasswordValidator:
+    def __call__(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Пароль должен быть длиной не менее 8 символов.")
+        special_chars = "!@#$%^&*()_+-={}[]|\\:;\"'<>,.?/~"
+        if not any(char in special_chars for char in value):
+            raise serializers.ValidationError("Пароль должен содержать хотя бы один специальный символ")
+        if not any(char.isupper() for char in value):
+            raise serializers.ValidationError("Пароль должен содержать хотя бы одну заглавную букву")
+        if not any(char.islower() for char in value):
+            raise serializers.ValidationError("Пароль должен содержать хотя бы одну строчную букву")
+        if not any(char.isdigit() for char in value):
+            raise serializers.ValidationError("Пароль должен содержать хотя бы одну цифру")
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,37 +22,19 @@ class UserSerializer(serializers.ModelSerializer):
     middle_name = serializers.CharField(max_length=150)
     organization = serializers.CharField(max_length=150)
     email = serializers.EmailField()
-    password = serializers.CharField(write_only=True, required=True)  # Добавлен атрибут required=True
+    password = serializers.CharField(write_only=True, required=True, validators=[PasswordValidator()])
 
     class Meta:
         model = UserModel
         fields = ('username', 'password', 'email', 'first_name', 'last_name', 'middle_name', 'organization')
 
-    def create(self, validate_data):
-        password = validate_data.pop("password")  # pop удаляет из словаря и возвращает значение элемента
-
-        # Валидация пароля
-        if len(password) < 8:
-            raise serializers.ValidationError("Пароль должен быть длиной не менее 8 символов.")
-        special_chars = "!@#$%^&*()_+-={}[]|\\:;\"'<>,.?/~"
-        if not any(char in special_chars for char in password):
-            raise serializers.ValidationError("Пароль должен содержать хотя бы один специальный символ")
-
-        if not any(char.isupper() for char in password):
-            raise serializers.ValidationError("Пароль должен содержать хотя бы одну заглавную букву")
-
-        if not any(char.islower() for char in password):
-            raise serializers.ValidationError("Пароль должен содержать хотя бы одну строчную букву")
-
-        if not any(char.isdigit() for char in password):
-            raise serializers.ValidationError("Пароль должен содержать хотя бы одну цифру")
-
-        raise serializers.ValidationError("Пароль валиден")
-
-        user = UserModel(**validate_data)  # распаковывает словарь и передает значение как именованные аргументы в
-        # конструктор класса User
-        user.set_password(password)  # метод класса User set_password устанавливает пароль пользователя
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = UserModel(**validated_data)
+        user.set_password(password)
         user.save()
         return user
+
+
 
 
