87fa82006407008cfd90e62526ce78164833c6fc
Eve <evaslobodyan8@gmail.com>
2024-04-26 12:10:26 +0700

    a new url has been added, as well as the ability to view user data and new get requests in swagger

--- backend/AuthService/CRUDAPI/regauth/views.py
+++ backend/AuthService/CRUDAPI/regauth/views.py
@@ -4,11 +4,21 @@ from rest_framework import generics
 from .serializer import UserSerializer
 from .models import UserModel
 from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserCreate(generics.CreateAPIView):
     queryset = UserModel.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
+
+class UserDetail(generics.RetrieveAPIView):
+        queryset = UserModel.objects.all()
+        serializer_class = UserSerializer
+        permission_classes = (IsAuthenticated,)
+
+        def get_object(self):
+            # Возвращаем текущего авторизованного пользователя
+            return self.request.user
 
 
