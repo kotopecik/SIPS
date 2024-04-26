4b62b1758c23cee4f7ef2c30811e0556d08d3795
Eve <evaslobodyan8@gmail.com>
2024-04-26 11:44:16 +0700

    now the black background is replaced with a transparent one before the tiles are cut

--- backend/TileService/Tiles.py
+++ backend/TileService/Tiles.py
@@ -7,12 +7,11 @@ import os
 
 def convert_to_rgba(input_file, output_file):
     # Команда для конвертации в формат RGBA
-    command = ['gdal_translate', '-of', 'VRT', '-expand', 'rgba', input_file, output_file]
+    command = ['gdal_translate', '-of', 'PNG', '-expand', 'rgba', input_file, output_file]
     subprocess.call(command)
 
 
-def remove_black(image_path):
-    image = Image.open(image_path)
+def remove_black(image):
     image = image.convert("RGBA")
     data = image.getdata()
 
@@ -25,26 +24,28 @@ def remove_black(image_path):
             new_data.append(item)
 
     image.putdata(new_data)
-    image.save(image_path, "PNG")
+    return image
 
 
 if __name__ == '__main__':
     freeze_support()
 
     # Входной и выходной файлы
-    input_file = r'C:\Users\User01\PycharmProjects\VRSDOP-service\backend\TileService\RastFilesInput\1fit.tif'
-    output_file = r'C:\Users\User01\PycharmProjects\VRSDOP-service\backend\TileService\RastFilesOutput\temp.vrt'
+    input_file = r'C:\Users\User01\PycharmProjects\VRSDOP-service\backend\TileService\RastFilesInput\2fit.tif'
+    output_file = r'C:\Users\User01\PycharmProjects\VRSDOP-service\backend\TileService\RastFilesOutput\temp.png'
 
+    # Конвертация в формат RGBA
     convert_to_rgba(input_file, output_file)
 
+    # Открытие изображения
+    image = Image.open(output_file)
+
+    # Замена черного фона на прозрачность
+    image = remove_black(image)
+
+    # Сохранение обработанного изображения
+    image.save(output_file, "PNG")
+
+    # Генерация тайлов
     gdal2tiles.generate_tiles(output_file, r'C:\Users\User01\PycharmProjects\VRSDOP-service\backend\TileService'
                                            r'\RastFilesOutput', nb_processes=2, zoom='7-9')
-
-    tiles_directory = r'C:\Users\User01\PycharmProjects\VRSDOP-service\backend\TileService\RastFilesOutput'
-
-    # Проход по всем файлам в директории тайлов
-    for root, dirs, files in os.walk(tiles_directory):
-        for file in files:
-            if file.endswith(".png"):
-                file_path = os.path.join(root, file)
-                remove_black(file_path)
