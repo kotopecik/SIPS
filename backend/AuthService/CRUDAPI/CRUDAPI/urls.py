c47133ea97ed965b3ccc2700e494b3c81927c6fc
Eve <evaslobodyan8@gmail.com>
2024-05-09 18:00:38 +0700

    lil update

--- backend/AuthService/CRUDAPI/CRUDAPI/urls.py
+++ backend/AuthService/CRUDAPI/CRUDAPI/urls.py
@@ -52,4 +52,3 @@ urlpatterns = [
         name='schema-redoc'
     ),
 ]
-
--- backend/AuthService/CRUDAPI/regauth/urls.py
+++ backend/AuthService/CRUDAPI/regauth/urls.py
@@ -1,12 +1,54 @@
-from django.urls import path
-from .views import (UserCreate)
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import (UserDetail)
+"""CRUDAPI1 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Authorization and registration REST API",
+        default_version='v1.0',
+        description="Service for providing authorization and registration of users",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+
+    permission_classes=[permissions.AllowAny, ],
+)
+
+main_route = "api/vAUTH/"
 
 urlpatterns = [
-    path("registration/", UserCreate.as_view(), name="registration"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_view"),
-    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh_view"),
-    path('user/', UserCreate.as_view(), name='user-create'),
-    path('user/<str:username>/', UserDetail.as_view(), name='user-detail'),
+    path('admin/', admin.site.urls),
+    path(f"{main_route}", include("regauth.urls")),
+    # path("api-auth/", include("rest_framework.urls")),
+    path(f"{main_route}doc", schema_view.as_view()),
+
+    path(
+        f"{main_route}swagger",
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+    path(
+        f"{main_route}redoc",
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'
+    ),
 ]
