a12669976efb16f9cd8a24ca67c5e7f98ff28808
SimpleIN1 <zimforshint_akross@mail.ru>
2024-06-13 00:33:33 +0700

    added arg push to push to docker hub, added docker puller

--- docker-images-builder.sh
+++ docker-images-builder.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # shellcheck disable=SC2164
+
+
+IS_PUSH=$1
+
+
 cd backend/;
 
 docker build -f AuthService/Dockerfile -t simplen1/auth-vrsdop AuthService;
@@ -18,3 +23,15 @@ cd ..;
 
 docker build -f frontend/Dockerfile -t simplen1/frontend-vrsdop frontend;
 
+if [[ $IS_PUSH == "push" ]]; then
+  docker push simplen1/auth-vrsdop
+  docker push simplen1/composite-data-vrsdop
+  docker push simplen1/info-composite-data-vrsdop
+  docker push simplen1/celery-vrsdop
+  docker push simplen1/frontend-vrsdop
+
+  # later
+#  docker push simplen1/loader-vrsdop
+#  docker push simplen1/data-processing-vrsdop
+#  docker push simplen1/tile-vrsdop
+fi
