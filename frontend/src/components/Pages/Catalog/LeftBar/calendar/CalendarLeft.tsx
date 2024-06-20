c3641ccc5628bfe946a850364ba1a8f593545454
DeadEndjke <perceptionarmy@icloud.com>
2024-06-21 02:34:31 +0700

    fix catalogsettings values

--- frontend/src/components/Pages/Catalog/LeftBar/calendar/CalendarLeft.tsx
+++ frontend/src/components/Pages/Catalog/LeftBar/calendar/CalendarLeft.tsx
@@ -6,27 +6,31 @@ import Select from '@mui/material/Select';
 import { convert, convertDates } from '@/utils/calendar';
 import { useAppDispatch, useAppSelector } from '@/hooks/hook';
 import { setEndDayS, setStartDayS } from '@/store/catalog/catalog-slice';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
 export const CalendarLeft = () => {
 
     const dates : string[] = useAppSelector(state => state.tile).dotdates
-    const [start, setStart] = useState<string>(convertDates(dates)[0].normal);
-    const [end, setEnd] = useState<string>(convertDates(dates)[convertDates(dates).length - 1].normal);
+    const [start, setStart] = useState<string>();
+    const [end, setEnd] = useState<string>();
+
+    const start_day : string = useAppSelector(state => state.catalog).start_day
+    const end_day : string = useAppSelector(state => state.catalog).end_day
 
     const dispatch = useAppDispatch();
-    const handleStart = (e) => {
-        dispatch(setStartDayS(e.target.value))
-        setStart(e.target.value)
-    }
 
+
+
+
+    const handleStart = (e) => {
+        dispatch(setStartDayS(e.target.value));
+    };
 
     const handleEnd = (e) => {
-        dispatch(setEndDayS(e.target.value))
-        setEnd(e.target.value)
-    }
+        dispatch(setEndDayS(e.target.value));
+    };
 
     return(
         <div className={s.calendarleft}>
@@ -38,7 +42,7 @@ export const CalendarLeft = () => {
                                 labelId="demo-simple-select-label"
                                 id="demo-simple-select"
                                 onChange={handleStart}
-                                value={start}
+                                value={start_day}
                             >
                                 {convertDates(dates).map((el) => (
                                     
@@ -57,6 +61,7 @@ export const CalendarLeft = () => {
                                 labelId="demo-simple-select-label"
                                 id="demo-simple-select"
                                 onChange={handleEnd}
+                                value={end_day}
                             >
                                 {convertDates(dates).map((el) => (
                                    
